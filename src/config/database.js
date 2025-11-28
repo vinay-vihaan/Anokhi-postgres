fe8627b98f1482a75e0f8f8b3d90c3fb587823c5
vinay-vihaan <vinayvihaandigitals@gmail.com>
2025-11-28 11:37:21 +0530

    connected render

--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,35 @@
 const { Sequelize } = require('sequelize');
-const config = require('./config');
+require('dotenv').config();
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    define: {
-      timestamps: false,
-    },
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false }, // Required for Render
+  },
+  define: {
+    timestamps: false, // your tables won't automatically get created_at/updated_at
+  },
+});
 
 module.exports = sequelize;
+
+// const { Sequelize } = require('sequelize');
+// const config = require('./config');
+
+// const env = process.env.NODE_ENV || 'development';
+// const dbConfig = config[env];
+
+// const sequelize = new Sequelize(
+//   dbConfig.database,
+//   dbConfig.username,
+//   dbConfig.password,
+//   {
+//     host: dbConfig.host,
+//     dialect: dbConfig.dialect,
+//     define: {
+//       timestamps: false,
+//     },
+//   }
+// );
+
+// module.exports = sequelize;
