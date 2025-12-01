2eab2f84ee30ea1e155accf33668d4dfc1898cd5
vinayvihaandigitals <vinayvihaandigitals@gmail.com>
2025-12-01 12:45:44 +0000

    added limit to 100

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // 5 requests per windowMs
+  max: 100, // 100 requests per windowMs
   message: 'Too many login/register attempts, please try again later',
   standardHeaders: true,
   legacyHeaders: false,
