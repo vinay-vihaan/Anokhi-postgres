82c2a1db2d95392f43ef3fe29e27dad39cd6e452
vinayvihaandigitals <vinayvihaandigitals@gmail.com>
2025-12-03 09:10:19 +0000

    uncommented user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,13 @@
-// const express = require('express');
-// const router = express.Router();
-// const userController = require('../controllers/userController');
-// const { authenticate } = require('../middleware/auth');
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const { authenticate } = require('../middleware/auth');
 
-// // User routes
-// router.get('/', /*authenticate,*/ userController.getAllUsers);
-// router.post('/', /*authenticate,*/ userController.createUser);
-// router.get('/:id', /*authenticate,*/ userController.getUserById);
-// router.put('/:id', /*authenticate,*/ userController.updateUser);
-// router.delete('/:id', /*authenticate,*/ userController.deleteUser);
+// User routes
+router.get('/', /*authenticate,*/ userController.getAllUsers);
+router.post('/', /*authenticate,*/ userController.createUser);
+router.get('/:id', /*authenticate,*/ userController.getUserById);
+router.put('/:id', /*authenticate,*/ userController.updateUser);
+router.delete('/:id', /*authenticate,*/ userController.deleteUser);
 
-// module.exports = router;
+module.exports = router;
